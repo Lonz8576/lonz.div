0618d5b81b55e7904a759a6619c36ab2cc38f5d2
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-10-10 13:30:57 -0400

    git graph

--- app/components/GitGraph.tsx
+++ app/components/GitGraph.tsx
@@ -1,4 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import GitHubCalendar from 'react-github-calendar';
 
-<GitHubCalendar username="Lonz8576" />
+
+
+const selectLastHalfYear = (contributions: any[]) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 6;
+  
+    return contributions.filter(activity => {
+      const date = new Date(activity.date);
+      const monthOfDay = date.getMonth();
+  
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
+  const explicitTheme= {dark : ['#a0eff3', '#4586ff', '#79c2d0', '#007cb9', '#01005e']};
+
+export default function GitCalendar() { 
+      return (
+        <GitHubCalendar username="Lonz8576" 
+          theme={explicitTheme}
+        transformData={selectLastHalfYear}  year={2024} showWeekdayLabels={true}  labels={{
+            totalCount: '{{count}} contributions in the last half year',}} />
+        )
+        }
+  
