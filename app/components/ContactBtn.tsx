5f5ee129b82840ac30ac00a484c89166add8de7e
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-10-16 17:30:03 -0400

    scr

--- app/components/ContactBtn.tsx
+++ app/components/ContactBtn.tsx
@@ -7,9 +7,9 @@ export default function ContactBtn () {
   return (
     <div>
         <form>
-    <iframe data-tally-src="https://tally.so/embed/nGJEpp?alignLeft=1&dynamicHeight=1" loading="lazy" width="100%" height="372" title="Contact
+    <iframe data-tally-src="https://tally.so/embed/nGJEpp?alignLeft=1&dynamicHeight=1"   loading="lazy" width="100%" height="372" title="Contact
 "  className='rounded-lg border-blue-100 border-2'id='Tally' ></iframe>
-        <Script async src="https://tally.so/widgets/embed.js" id='Tally'   onLoad={(e) => e.target.contentWindow.load()}  
+        <Script async src="https://tally.so/widgets/embed.js" id='Tally'  onLoad={(e) => e.target.contentWindow.load()}  
             />
     </form>
     </div>
