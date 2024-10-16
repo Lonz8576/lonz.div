6f2580f7eb354e4f80b10831ddb6e052243808f8
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2024-10-16 17:12:42 -0400

    contact

--- app/components/ContactBtn.tsx
+++ app/components/ContactBtn.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import React from 'react'
 import Script from "next/script";
 
@@ -6,11 +6,12 @@ export default function ContactBtn () {
     
   return (
     <div>
+        <form>
     <iframe data-tally-src="https://tally.so/embed/nGJEpp?alignLeft=1&dynamicHeight=1" loading="lazy" width="100%" height="372" title="Contact
-"  className='rounded-lg border-blue-100 border-2'></iframe>
-        <Script async src="https://tally.so/widgets/embed.js"         
+"  className='rounded-lg border-blue-100 border-2'id='Tally' ></iframe>
+        <Script async src="https://tally.so/widgets/embed.js" id='Tally'   onLoad={(e) => e.target.contentWindow.load()}  
             />
-    
+    </form>
     </div>
   )
 }
