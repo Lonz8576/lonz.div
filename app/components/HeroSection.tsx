4f812a63654ab6c93dd883a755734b5276bb2cff
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2025-02-10 17:04:37 -0500

    update main page
    screen size

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -72,7 +72,7 @@ const Hero = () => {
                  
         </div>
         </div>
-        <div className=' images ms-8 '>
+        <div className=' images ms-8 sm:invisible'>
           <picture>
         <img src= 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/128/064/original/20211002_105331-EDIT-EDIT.jpg?1717359345'  className='flex headshot relative -top-20 shadow-lg items-center justify-between' alt='Nics headshot' />
         </picture>
