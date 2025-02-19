9d1c7af4c292f99936650bebc1464fd780de3a36
Nic Alonso <167815213+Lonz8576@users.noreply.github.com>
2025-02-19 14:37:08 -0500

    component deleted

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,4 +1,4 @@
-import { HoverEffect } from "../components/HoverEffect"
+
 
 export default function aboutLayout({
     children, // will be a page or nested layout
@@ -8,10 +8,7 @@ export default function aboutLayout({
    
     return (
       <div className="relative bg-black-100 flex justify-center items-center h-full pb-80  overflow-hidden mx-auto sm:px-10 px-5">
-        <div className="max-w-lg w-1/2  text-wrap justify-center -top-72   mb-20 sm:hidden lg:flex relative -rotate-90">
-         <HoverEffect text='my background' duration={20} />
-
-         </div>
+   
         
     
          {children}
